732da5024dd22e3b3a13273904694ded8d6be0c4
Alexey Soshin <alexey.soshin@deliveroo.co.uk>
2021-10-21 14:54:32 +0100

    Adding runnable examples to code that was missing them in Chapter 2 (#11)

--- Chapter2/src/main/kotlin/1_Singleton.kt
+++ Chapter2/src/main/kotlin/1_Singleton.kt
@@ -1,14 +1,21 @@
+
+fun main() {
+    Logger.log("Hello World")
+}
+
 /**
  * Object keyword will create a Singleton
  */
 object Logger {
-
     init {
         println("I was accessed for the first time")
 
         // Initialization logic goes here
     }
 
+    fun log(message: String) {
+        println("Logging $message")
+    }
     // More code goes here
 }
 
--- Chapter2/src/main/kotlin/5_Prototype.kt
+++ Chapter2/src/main/kotlin/5_Prototype.kt
@@ -1,11 +1,17 @@
+fun main() {
+    val originalUser = User("admin1", Role.ADMIN, setOf("READ", "WRITE", "DELETE"))
+    allUsers += originalUser
+
+    createUser("admin2", Role.ADMIN)
+
+    println(allUsers)
+}
+
+
 enum class Role {
-
     ADMIN,
-
     SUPER_ADMIN,
-
     REGULAR_USER
-
 }
 
 data class User(
@@ -14,11 +20,11 @@ data class User(
     val permissions: Set<String>,
 ) {
     fun hasPermission(permission: String) = permission in permissions
-
 }
-private val allUsers = mutableListOf<User>()
-fun createUser(_name: String, role: Role) {
 
+private val allUsers = mutableListOf<User>()
+
+fun createUser(_name: String, role: Role) {
     for (u in allUsers) {
         if (u.role == role) {
             allUsers += u.copy(name = _name)
