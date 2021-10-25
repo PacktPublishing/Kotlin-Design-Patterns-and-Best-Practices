09227b77af76a0bb5a7a5abc92d491a49563b685
Alexey Soshin <alexey.soshin@gmail.com>
2021-10-25 15:18:33 +0100

    Chapter 11 example

---
+++ Chapter11/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "me.soshin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val vertxVersion = "4.1.5"
+dependencies {
+    implementation("io.vertx:vertx-core:$vertxVersion")
+    implementation("io.vertx:vertx-web:$vertxVersion")
+    implementation("io.vertx:vertx-web-client:$vertxVersion")
+    implementation("io.vertx:vertx-lang-kotlin:$vertxVersion")
+    implementation("io.vertx:vertx-lang-kotlin-coroutines:$vertxVersion")
+    implementation("io.vertx:vertx-pg-client:$vertxVersion")
+    implementation("org.postgresql:postgresql:42.3.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ Chapter11/src/main/kotlin/server.kt
@@ -0,0 +1,71 @@
+import io.vertx.core.Vertx
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.kotlin.core.json.json
+import io.vertx.kotlin.core.json.obj
+import io.vertx.kotlin.coroutines.CoroutineVerticle
+import kotlinx.coroutines.launch
+
+fun main() {
+    val vertx = Vertx.vertx()
+    vertx.deployVerticle(ServerVerticle())
+    vertx.deployVerticle(CatsVerticle())
+}
+
+class ServerVerticle : CoroutineVerticle() {
+    override suspend fun start() {
+        val router = router()
+
+        vertx.createHttpServer()
+            .requestHandler(router)
+            .listen(8081)
+        println("open http://localhost:8081")
+    }
+
+    private fun router(): Router {
+        // Our router code comes here now
+
+        val router = Router.router(vertx)
+        router.route().handler(BodyHandler.create())
+        router.get("/status").handler { ctx ->
+            val json = json {
+                obj(
+                    "status" to "OK"
+                )
+            }
+
+            ctx.response()
+                .setStatusCode(200)
+                .end(json.toString())
+        }
+        router.mountSubRouter("/cats", catsRouter())
+
+        return router
+    }
+
+    private fun catsRouter(): Router {
+        val router = Router.router(vertx)
+        router.delete("/:id").handler { ctx ->
+            val id = ctx.request().getParam("id").toInt()
+            vertx.eventBus().request<Int>("cats:delete", id) {
+                ctx.end()
+            }
+        }
+        router.put("/:id").handler { ctx ->
+            launch {
+                val id = ctx.request().getParam("id").toInt()
+                val body: JsonObject = ctx.bodyAsJson.mergeIn(json {
+                    obj("id" to id)
+                })
+
+                vertx.eventBus().request<Int>("cats:update", body) { res ->
+                    ctx.end(res.result().body().toString())
+                }
+            }
+        }
+        return router
+    }
+}
+
+
