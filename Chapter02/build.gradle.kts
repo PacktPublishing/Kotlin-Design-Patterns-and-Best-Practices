05290d2d32ac3ffdc969bbd169312bbcce150876
Alexey Soshin <alexey.soshin@gmail.com>
2021-12-10 23:58:23 +0000

    Chapter 02 Kotlin Update

--- Chapter02/build.gradle.kts
+++ Chapter02/build.gradle.kts
@@ -1,24 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.6.0"
 }
 
-group = "com.depop"
+group = "me.soshin"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile>() {
